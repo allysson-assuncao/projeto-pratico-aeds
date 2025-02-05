4a098712ead733d1523fb3be277ebbcbf920e11e
allysson-assuncao <anybody723@proton.me>
2025-02-05 16:40:49 -0300

    finished part of the changes and corrections

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.Random;
 
-interface SortAlgorithm {
+/*interface SortAlgorithm {
     void sort(int[] array);
 
     int getSwapCount();
@@ -243,6 +242,168 @@ class QuickSort implements SortAlgorithm {
     public int getComparisonCount() {
         return comparisonCount;
     }
+}*/
+
+interface SortAlgorithm {
+    void sort(int[] array);
+
+}
+
+class InsertionSort implements SortAlgorithm {
+    public void sort(int[] array) {
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while (i >= 0 && array[i] > key) {
+                array[i + 1] = array[i];
+                i = i - 1;
+            }
+            array[i + 1] = key;
+        }
+    }
+}
+
+class SelectionSort implements SortAlgorithm {
+    public void sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
+
+class BubbleSort implements SortAlgorithm {
+    public void sort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+}
+
+class MergeSort implements SortAlgorithm {
+    public void sort(int[] array) {
+        mergeSort(array, 0, array.length - 1);
+    }
+
+    private void mergeSort(int[] array, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            mergeSort(array, left, middle);
+            mergeSort(array, middle + 1, right);
+            merge(array, left, middle, right);
+        }
+    }
+
+    private void merge(int[] array, int left, int middle, int right) {
+        int leftIndex = left, rightIndex = middle + 1, auxIndex = 0;
+        int[] auxArray = new int[right - left + 1];
+
+        while (leftIndex <= middle && rightIndex <= right) {
+            if (array[leftIndex] <= array[rightIndex]) {
+                auxArray[auxIndex] = array[leftIndex];
+                leftIndex++;
+            } else {
+                auxArray[auxIndex] = array[rightIndex];
+                rightIndex++;
+            }
+            auxIndex++;
+        }
+
+        while (leftIndex <= middle) {
+            auxArray[auxIndex] = array[leftIndex];
+            auxIndex++;
+            leftIndex++;
+        }
+
+        while (rightIndex <= right) {
+            auxArray[auxIndex] = array[rightIndex];
+            auxIndex++;
+            rightIndex++;
+        }
+
+        for (auxIndex = 0; auxIndex < auxArray.length; auxIndex++) {
+            array[left + auxIndex] = auxArray[auxIndex];
+        }
+    }
+}
+
+class QuickSort implements SortAlgorithm {
+    private static final int INSERTION_SORT_THRESHOLD = 10;
+
+    public void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(int[] array, int low, int high) {
+        while (low < high) {
+            if (high - low < INSERTION_SORT_THRESHOLD) {
+                insertionSort(array, low, high);
+                break;
+            } else {
+                int pi = partition(array, low, high);
+                if (pi - low < high - pi) {
+                    quickSort(array, low, pi - 1);
+                    low = pi + 1;
+                } else {
+                    quickSort(array, pi + 1, high);
+                    high = pi - 1;
+                }
+            }
+        }
+    }
+
+    private int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private void insertionSort(int[] array, int low, int high) {
+        for (int i = low + 1; i <= high; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= low && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
 
 public class Main {
@@ -256,8 +417,8 @@ public class Main {
         for (int size : sizes) {
             for (String order : orders) {
                 double totalDuration = 0;
-                int totalSwaps = 0;
-                int totalComparisons = 0;
+                /*int totalSwaps = 0;
+                int totalComparisons = 0;*/
 
                 for (int run = 0; run < TEST_RUNS; run++) {
                     int[] array = generateArray(size, order, seed + run);
@@ -265,15 +426,15 @@ public class Main {
                     algorithm.sort(array);
                     long endTime = System.nanoTime();
                     totalDuration += (endTime - startTime) / 1_000_000.0;
-                    totalSwaps += algorithm.getSwapCount();
-                    totalComparisons += algorithm.getComparisonCount();
+                    /*totalSwaps += algorithm.getSwapCount();
+                    totalComparisons += algorithm.getComparisonCount();*/
                 }
 
                 double avgDuration = totalDuration / TEST_RUNS;
-                int avgSwaps = totalSwaps / TEST_RUNS;
-                int avgComparisons = totalComparisons / TEST_RUNS;
+                /*int avgSwaps = totalSwaps / TEST_RUNS;
+                int avgComparisons = totalComparisons / TEST_RUNS;*/
 
-                csvWriter.append(String.format("%s,%d,%s,%.3f,%d,%d\n", algorithmName, size, order, avgDuration, avgSwaps, avgComparisons));
+                csvWriter.append(String.format("%s,%d,%s,%.3f\n", algorithmName, size, order, avgDuration));
             }
         }
     }
